07cd9df5ec68a34cbfedc4c3692c11569ab046b0
sherzock <jacobo.galofre@gmail.com>
2020-01-21 18:39:10 +0100

    Main menu

---
+++ Altera_Vita/Assets/Scripts/ButtonsBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsBehavior : MonoBehaviour
+{
+    void NewGame()
+    {
+        
+    }
+
+    void HowToPlay()
+    {
+
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+}
