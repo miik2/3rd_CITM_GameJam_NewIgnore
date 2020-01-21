fa87f98e64d7b4585ccbe2a7f282e51ff1de1d06
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2020-01-21 21:17:44 +0100

    Implemented animations to every 4 directions

--- Altera_Vita/Assets/Scripts/PlayerController.cs
+++ Altera_Vita/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 hipsOffset;
+    [SerializeField] private Vector3 chestOffset;
     private float lastTime = 0.0f;
     public float waitTime = 2.0f;
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
     private Transform chest;
+    private Transform hips;
     private float Input_X;
     private float Input_Z;
 
@@ -25,16 +27,19 @@ public class PlayerController : MonoBehaviour
     {
         animator = gameObject.GetComponent<Animator>();
         chest = animator.GetBoneTransform(HumanBodyBones.Chest);
+        hips = animator.GetBoneTransform(HumanBodyBones.Hips);
         health = maxHealth;
     }
 
     void Update()
     {
-        if (IsDead() || Input.GetKeyDown(KeyCode.R)) Reset();
+        if (IsDead() || Input.GetKeyDown(KeyCode.R)) ResetCharacter();
 
         if (Time.time >= lastTime + waitTime)
         {
             // Player movement
+            Input_X = Input.GetAxis("Vertical");
+            animator.SetFloat("Input_X", Input_X);
             Input_Z = Input.GetAxis("Vertical");
             animator.SetFloat("Input_Z", Input_Z);
 
@@ -53,10 +58,14 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 transform.position += new Vector3(-speed, 0, 0);
+                Input_X = Input.GetAxis("Horizontal");
+                animator.SetFloat("Input_X", Input_X);
             }
             if (Input.GetKey(KeyCode.D))
             {
                 transform.position += new Vector3(speed, 0, 0);
+                Input_X = Input.GetAxis("Horizontal");
+                animator.SetFloat("Input_X", Input_X);
             }
         }
     }
@@ -64,7 +73,10 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {        
         chest.LookAt(target);
-        chest.rotation = chest.rotation * Quaternion.Euler(offset);
+        chest.rotation *= Quaternion.Euler(chestOffset);
+
+       // hips.LookAt(CalculateForwardTransform());        
+       // hips.rotation *= Quaternion.Euler(hipsOffset);
     }
 
     public bool IsDead()
@@ -72,11 +84,12 @@ public class PlayerController : MonoBehaviour
         return health <= 0;
     }
 
-    private void Reset()
+    private void ResetCharacter()
     {
         restarting = true;
         lastTime = Time.time;
         transform.position = startingPosition;
         // Clone code
     }
+
 }
