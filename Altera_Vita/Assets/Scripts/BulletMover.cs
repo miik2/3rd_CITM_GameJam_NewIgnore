8cf0d5a35b50d51f99ba24112f10f556c994f6d7
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2020-01-22 05:02:17 +0100

    minor

--- Altera_Vita/Assets/Scripts/BulletMover.cs
+++ Altera_Vita/Assets/Scripts/BulletMover.cs
@@ -3,14 +3,11 @@
 public class BulletMover : MonoBehaviour
 {
     public float speed;
-    private Rigidbody rig;
 
     Vector3 direction;
 
     private void Awake()
     {
-        rig = GetComponent<Rigidbody>();
-
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         float midPoint = (transform.position - Camera.main.transform.position).magnitude;
 
