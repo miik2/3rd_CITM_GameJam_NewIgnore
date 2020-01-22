850abf8cf81263bd168b7e0e2947e0968f64d453
sherzock <jacobo.galofre@gmail.com>
2020-01-22 04:21:50 +0100

    Now title has animation

--- Altera_Vita/Assets/Scripts/ButtonsBehavior.cs
+++ Altera_Vita/Assets/Scripts/ButtonsBehavior.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class ButtonsBehavior : MonoBehaviour
 {
     public Transition trans;
+    public TitleAnim titleanim;
 
     public void NewGame()
     {
+        StartCoroutine(NewGameWait());   
+    }
+
+    IEnumerator NewGameWait()
+    {
+        titleanim.Animout();
+        yield return new WaitForSeconds(2f);
         trans.LoadNextLevel();
-        
     }
 
     public void HowToPlay()
@@ -20,10 +27,16 @@ public class ButtonsBehavior : MonoBehaviour
 
     IEnumerator HowToPlayWait()
     {
+        titleanim.Animout();
+        yield return new WaitForSeconds(2f);
         trans.NormalTransitionStart();
         yield return new WaitForSeconds(1f);
-        GameObject.Find("Title").GetComponent<Image>().enabled = false;
+        //GameObject.Find("Title").GetComponent<Image>().enabled = false;
         GameObject.FindGameObjectWithTag("NG").GetComponent<Text>().enabled = false;
+        GameObject.FindGameObjectWithTag("Altera").GetComponent<Image>().enabled = false;
+        GameObject.FindGameObjectWithTag("ViTA").GetComponent<Image>().enabled = false;
+        GameObject.FindGameObjectWithTag("Line1").GetComponent<Image>().enabled = false;
+        GameObject.FindGameObjectWithTag("Line2").GetComponent<Image>().enabled = false;
         GameObject.FindGameObjectWithTag("EX").GetComponent<Text>().enabled = false;
         GameObject.FindGameObjectWithTag("HTP").GetComponent<Text>().enabled = false;
         GameObject.Find("Controls").GetComponent<Image>().enabled = true;
@@ -41,10 +54,15 @@ public class ButtonsBehavior : MonoBehaviour
         yield return new WaitForSeconds(1f);
         GameObject.Find("Controls").GetComponent<Image>().enabled = false;
         GameObject.FindGameObjectWithTag("BK").GetComponent<Text>().enabled = false;
-        GameObject.Find("Title").GetComponent<Image>().enabled = true;
+        //GameObject.Find("Title").GetComponent<Image>().enabled = true;
+        GameObject.FindGameObjectWithTag("Altera").GetComponent<Image>().enabled = true;
+        GameObject.FindGameObjectWithTag("ViTA").GetComponent<Image>().enabled = true;
+        GameObject.FindGameObjectWithTag("Line1").GetComponent<Image>().enabled = true;
+        GameObject.FindGameObjectWithTag("Line2").GetComponent<Image>().enabled = true;
         GameObject.FindGameObjectWithTag("NG").GetComponent<Text>().enabled = true;
         GameObject.FindGameObjectWithTag("EX").GetComponent<Text>().enabled = true;
         GameObject.FindGameObjectWithTag("HTP").GetComponent<Text>().enabled = true;
+        titleanim.Animin();
     }
 
     public void Exit()
