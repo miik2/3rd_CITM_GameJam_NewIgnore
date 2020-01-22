585c1b7d89a05f91642ae25bf6922b2e892d4969
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2020-01-22 10:56:16 +0100

    added custom cursor

---
+++ Altera_Vita/Assets/ShowMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMouse : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude;
+
+        Vector3 destination = mouseRay.origin + mouseRay.direction * midPoint;
+        destination.y = 10;
+
+        transform.position = destination;
+    }
+}
